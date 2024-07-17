31840333db43385e7cf93829357fc6f1dc8e822d
Biniyamseid <Biniyamseid3@gmail.com>
2024-07-17 05:24:29 +0200

    get pdf text

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import langchain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+from htmlTemplates import css, bot_template, user_template
+
+
+from langchain.llms import HuggingFaceHub
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
 
 
 if __name__ == '__main__':
